70e665615626923e8e6752792c2dfbae82e40af8
wonny-log <hee940727@gmail.com>
2018-07-24 18:38:55 +0900

    function component 를 component 로 변경

--- pages/cs.js
+++ pages/cs.js
@@ -1,23 +1,27 @@
 import Layout from '../components/Layout'
 import FaqHeader from '../components/FaqHeader'
-export default () => (
-    <Layout>
-        <div id="faqcontent">
-            <FaqHeader/>
-        </div>
-       <style jsx>
-       {`
-       #faqcontent {
 
-       }
-       @media (min-width:1024px){
-        #faqcontent {
-            width:80%;
-            margin:0px auto;
-        }
-       }
-       `}
-       </style>
-        
-    </Layout>
-)
+export default class CS extends React.Component {
+  render() {
+      return (
+        <Layout>
+            <div id="faqcontent">
+                <FaqHeader/>
+            </div>
+            <style jsx>
+            {`
+            #faqcontent {
+
+            }
+            @media (min-width:1024px){
+            #faqcontent {
+                width:80%;
+                margin:0px auto;
+            }
+            }
+            `}
+            </style>
+        </Layout>
+    )
+  }
+}
